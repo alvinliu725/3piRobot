473f53027d4829f1e5979bae0c8a62150f5b5d11
alvinliu725 <yan.liu@mun.ca>
2014-05-31 00:37:33 -0230

    Update mainConsole.java

--- ECE-3piRobot/src/test/mainConsole.java
+++ ECE-3piRobot/src/test/mainConsole.java
@@ -1,18 +1,23 @@
-package test;
 
 
- import java.awt.*;  
+import java.awt.*;  
 import java.awt.image.BufferedImage;  
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;  
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;  
+import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;  
-import test.mainConsole;
+
+import console.ButtonPanel;
 
  public class mainConsole extends JPanel{  
-   private static final long serialVersionUID = 1L;  
-   private BufferedImage image;  
+
+   private BufferedImage image = null;  
    // Create a constructor method  
    public mainConsole(){  
      super();  
@@ -24,80 +29,59 @@ import test.mainConsole;
      image=newimage;  
      return;  
    }  
-   /**  
-    * Converts/writes a Mat into a BufferedImage.  
-    *  
-    * @param matrix Mat of type CV_8UC3 or CV_8UC1  
-    * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY  
-    */  
-   public static BufferedImage matToBufferedImage(Mat matrix) {  
-     int cols = matrix.cols();  
-     int rows = matrix.rows();  
-     int elemSize = (int)matrix.elemSize();  
-     byte[] data = new byte[cols * rows * elemSize];  
-     int type;  
-     matrix.get(0, 0, data);  
-     switch (matrix.channels()) {  
-       case 1:  
-         type = BufferedImage.TYPE_BYTE_GRAY;  
-         break;  
-       case 3:  
-         type = BufferedImage.TYPE_3BYTE_BGR;  
-         // bgr to rgb  
-         byte b;  
-         for(int i=0; i<data.length; i=i+3) {  
-           b = data[i];  
-           data[i] = data[i+2];  
-           data[i+2] = b;  
-         }  
-         break;  
-       default:  
-         return null;  
-     }  
-     BufferedImage image2 = new BufferedImage(cols, rows, type);  
-     image2.getRaster().setDataElements(0, 0, cols, rows, data);  
-     return image2;  
-   }  
+ 
    public void paintComponent(Graphics g){  
-      BufferedImage temp=getimage();  
-      g.drawImage(temp,10,10,temp.getWidth(),temp.getHeight(), this);  
+      BufferedImage temp=getimage(); 
+      if(temp!=null)
+         g.drawImage(temp,10,10,temp.getWidth()+5,temp.getHeight()+5, this);  
    }  
-  
-     public static void main(String arg[]){  
+   
+   
+   public static void main(String arg[]){  
     // Load the native library.  
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);    
-    JFrame frame = new JFrame("BasicPanel");  
+    JFrame frame = new JFrame("3piRobot");  
+    frame.setLayout(new FlowLayout());
+    frame.setVisible(true); 
+    frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-    frame.setSize(640,480);  
-    frame.setResizable(true);
+    frame.setSize(1200,800);  
     mainConsole panel = new mainConsole();  
-    frame.setContentPane(panel);       
-    frame.setVisible(true);       
+    frame.setContentPane(panel);  
+    //ButtonPanel buttonP = new ButtonPanel();
+   // panel.add(buttonP);
     Mat webcam_image=new Mat();  
-    BufferedImage temp;  
-    VideoCapture capture =new VideoCapture(1);  
-    if( capture.isOpened())  
-     {  
+    BufferedImage temp=null;  
+    VideoCapture capture = new VideoCapture(0);  
+
+    if(capture.isOpened())  
+     {
       while( true )  
-      {  
+      {    
         capture.read(webcam_image);  
         if( !webcam_image.empty() )  
          {  
-           System.out.println("Camera Connection OK - Stream Testing.............");
-           frame.setSize(webcam_image.width()+40,webcam_image.height()+60);  
-           temp=matToBufferedImage(webcam_image);  
+           Highgui.imwrite("cameraTemp.jpg", webcam_image);
+           try {
+			temp=ImageIO.read(new File("cameraTemp.jpg"));
+		   } 
+           catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Read the camera failed");
+		   }
            panel.setimage(temp);  
            panel.repaint();  
          }  
          else  
          {  
-           System.out.println(" --(!) No captured frame -- Break!");  
+           System.out.println("Camera Issues! Please check the connection");  
            break;  
          }  
         }  
-       }  
-       return;  
+       } 
+       
    }  
+
  }  
  
 
